d009b8ff2e6a0222469a05ed19988d3ad8d11595
koba-masa <kobayashi_mshr@koba-masa.com>
2024-01-03 16:57:23 +0900

    feat: ヘッダー部とボディ部を作成する

--- src/components/css_selector.tsx
+++ src/components/css_selector.tsx
@@ -1,7 +1,42 @@
 import { useState } from "react";
+import "./css_selector.scss";
 
 export default function CssSelector() {
-  const noElementsFoundMessage: Array<string> = ["No elements found."];
+  const noElementsFoundMessage: Array<string> = [
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+    "No elements found.",
+  ];
 
   const [inputValue, setInputValue] = useState("");
   const [results, setResults] = useState<Array<string>>(noElementsFoundMessage);
@@ -34,8 +69,9 @@ export default function CssSelector() {
   };
 
   return (
-    <section>
-      <div>
+    <section className="cssSelector">
+      <div className="input">
+        <label htmlFor="selector">CSS Selector:</label>
         <input
           type="text"
           name="selector"
@@ -43,12 +79,16 @@ export default function CssSelector() {
           onChange={handleInputChange}
         />
       </div>
-      <div>
-        {
-          results.map((result, index) => (
-            <p key={index}>{result}</p>
-          ))
-        }
+      <div className="results">
+        <div className="head">
+        </div>
+        <div className="body">
+          {
+            results.map((result, index) => (
+              <div key={index}>{result}</div>
+            ))
+          }
+        </div>
       </div>
     </section>
   );
